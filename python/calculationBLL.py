357ddd0bc6d69a43b906e70c17dd615ee033c092
Yaryna <p.yaryna@gmail.com>
2022-02-24 14:28:39 +0100

    add calculation structure

---
+++ python/calculationBLL.py
@@ -0,0 +1,12 @@
+from calculationDAL import *
+
+def printDal():
+    # rates = getCollection('rates')
+    rates = GetRatesForPreviousDay()
+    print(rates)
+
+def calculateSimilarity():
+    print()
+
+def calculateRecomendations():
+    print()
