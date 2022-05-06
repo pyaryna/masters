ab4f1a96a7ea5627820d69914473ce3f7fdb623d
Yaryna <p.yaryna@gmail.com>
2022-05-06 14:04:04 +0200

    add swagger

--- python/calculation_controller.py
+++ python/calculation_controller.py
@@ -1,56 +1,63 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, jsonify
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 from flask_cors import CORS
+from bson.objectid import ObjectId
+from flask_restx import Api, Resource
 
 from config import *
 from calculation_bll import *
 
 app = Flask(__name__)
+api = Api(app)
+
 client = MongoClient(connectionString)
-# db = client.lin_flask
 db = client['masters']
 CORS(app)
 
 connect_to_database(db)
 
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+@api.route('/')
+class Home(Resource):
+    def get(self):
+        return '''<h1>Recommendation system of books</h1>
+            <p>Item-based Collaborative filtration</p>'''
 
 
-@app.route('/api/v1/similarity', methods=['GET'])
-def calculate_book_similarity():
-    amount = calculate_similarity()
-    return jsonify({
-            'status': '200 OK',
-            'message':'Data is posted to MongoDB!',
-            'amount': amount
-        })
+@api.route('/similarity')
+class BookSimilarity(Resource):
+    def get(self):
+        amount = calculate_similarity()
+        return jsonify({
+                'status': '200 OK',
+                'message':'Data is posted to MongoDB!',
+                'amount': amount
+            })
 
 
-@app.route('/api/v1/user', methods=['GET'])
-def recommendation_for_user():
-    recommedations = []
-    for item in calculate_recomendations_for_user():
-        recommedations.append({
-            'rate': item[0],
-            'bookId': str(item[1])
-        })
-    return jsonify(recommedations)
+@api.route('/user/<string:id>')
+class RecommendationForUser(Resource):
+    def get(self, id):
+        recommedations = []
+        user_id = ObjectId(id) # 620bb9fef23b1bc78052c605
+        for item in calculate_recomendations_for_user(user_id):
+            recommedations.append({
+                'rate': item[0],
+                'bookId': str(item[1])
+            })
+        return jsonify(recommedations)
 
 
-@app.route('/api/v1/book', methods=['GET'])
-def recommendation_by_book():
-    recommedations = []
-    for item in calculate_recomendations_by_book():
-        recommedations.append({
-            'rate': item[0],
-            'bookId': str(item[1])
-        })
-    return jsonify(recommedations)
+@api.route('/book/<string:id>')
+class RecommendationByBook(Resource):
+    def get(self, id):
+        recommedations = []
+        bookd_id = ObjectId(id) # 620ea6721feee707fc54939a
+        for item in calculate_recomendations_by_book(bookd_id):
+            recommedations.append({
+                'rate': item[0],
+                'bookId': str(item[1])
+            })
+        return jsonify(recommedations)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug = True)
